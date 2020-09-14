3508661c4e8cb7a3b9cac1e74d30dc8753d5fed2
Bogi Kiss <bogikiss85@gmail.com>
2020-09-14 10:37:28 +0200

    add RetrofitClient.kt

--- app/src/main/java/com/codecool/holabusz/network/RetrofitClient.kt
+++ app/src/main/java/com/codecool/holabusz/network/RetrofitClient.kt
@@ -1,5 +1,7 @@
 package com.codecool.holabusz.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -8,18 +10,36 @@ class RetrofitClient {
 
     companion object {
         const val BASE_URL = "https://futar.bkk.hu/api/query/v1/ws/otp/api/where/"
+        var retrofit: Retrofit? = null
 
-        fun create(): RequestApi {
+        fun getClient(): Retrofit {
 
-            val retrofit = Retrofit.Builder()
-                .addCallAdapterFactory(
-                    RxJava2CallAdapterFactory.create())
-                .addConverterFactory(
-                    GsonConverterFactory.create())
-                .baseUrl(BASE_URL)
-                .build()
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient()
 
-            return retrofit.create(RequestApi::class.java)
+            if (retrofit == null) {
+
+                retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(
+                        RxJava2CallAdapterFactory.create()
+                    )
+                    .addConverterFactory(
+                        GsonConverterFactory.create()
+                    )
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .build()
+
+            }
+
+
+            return retrofit!!
+        }
+
+        fun getRequestApi(): RequestApi {
+            return RetrofitClient.getClient().create(RequestApi::class.java)
         }
     }
+
 }
