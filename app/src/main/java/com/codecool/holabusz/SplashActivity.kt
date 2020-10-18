db93ab1885d0df9683b65848c81cf02257ac3859
Bogi Kiss <bogikiss85@gmail.com>
2020-10-18 12:48:17 +0200

    add splash screen with animation

---
+++ app/src/main/java/com/codecool/holabusz/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.codecool.holabusz
+
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.marginTop
+import com.codecool.holabusz.main.MainActivity
+import gr.net.maroulis.library.EasySplashScreen
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
