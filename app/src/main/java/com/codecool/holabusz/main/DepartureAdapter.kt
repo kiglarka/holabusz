c35112e304135671cc28aa72ad56547c19d42051
Bogi Kiss <bogikiss85@gmail.com>
2020-11-25 15:11:25 +0100

    remove unnecessary line

--- app/src/main/java/com/codecool/holabusz/main/DepartureAdapter.kt
+++ app/src/main/java/com/codecool/holabusz/main/DepartureAdapter.kt
@@ -1,7 +1,6 @@
-package com.codecool.holabusz.main
+    package com.codecool.holabusz.main
 
 import android.graphics.Color
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
